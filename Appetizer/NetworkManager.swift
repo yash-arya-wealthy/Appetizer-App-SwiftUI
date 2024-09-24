f391f79e021b7c56303538843dbd6b32554755cc
Yash Arya <wealthy@Yash-Arya.local>
2024-09-24 22:47:54 +0530

    Updated the Network Manager

--- Appetizer/NetworkManager.swift
+++ Appetizer/NetworkManager.swift
@@ -11,11 +11,41 @@ final class NetworkManager {
     static let shared = NetworkManager()
     
     static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
-    static let appetizerURL = baseURL + "appetizers"
-    
-    func getAppetizers(completed: @escaping (Result<[Appetizer], APError>) -> Void) {
-        
-    }
+    private let appetizerURL = baseURL + "appetizers"
     
     private init() {}
+    
+    func getAppetizers(completed: @escaping (Result<[Appetizer], APError>) -> Void) {
+        guard let url = URL(string: appetizerURL) else {
+            completed(.failure(.invalidURL))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            guard let _ = error else {
+                completed(.failure(.unableToComplete))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completed(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let decodedResponse = try decoder.decode(AppetizerResponse.self, from: data)
+                completed(.success(decodedResponse.request))
+            } catch {
+                completed(.failure(.invalidData))
+            }
+        }
+        task.resume()
+    }
+    
 }
